2e3e7ba25dc15e208b858a205d513ca3e023ce27
Eril-Nakiri <122957034+Eril-Nakiri@users.noreply.github.com>
2025-12-11 11:10:19 +0700

    Update Catalogue.h

--- Game-Catalogue/Catalogue.h
+++ Game-Catalogue/Catalogue.h
@@ -44,7 +44,15 @@ struct ListGame {
     adrGame last;
 };
 
-struct ListCharacter {
-	adrCharacter first;
-};
+void createLIstGame(ListGame &G)
+bool isEmptyGame(ListGame G);
+bool isEmptyCharacter(adrCharacter c);
+adrGame createElementGame(string judul, string genre, int tahunRilis, float rating);
+adrCharacter createElementCharacter(string nama, string asalDunia, string ras, string peran, int strenght, int agility, int hp, int battleIq, int experience);
+void addMahasiswa(ListMahasiswa &L, adrMahasiswa p);
+void addMatakuliah(adrMahasiswa p, adrMataKuliah q);
+adrMahasiswa searchMahasiswa(ListMahasiswa L, string nama, string nim);
+void displayList(ListMahasiswa L);
+
+
 #endif // CATALOGUE_H_INCLUDED
