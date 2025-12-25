d42a0ce3f7e3826ae8177b2e0d2385f114725932
Eril-Nakiri <122957034+Eril-Nakiri@users.noreply.github.com>
2025-12-25 16:48:47 +0700

    update is fs catalogue.cpp

--- Game-Catalogue/Catalogue.cpp
+++ Game-Catalogue/Catalogue.cpp
@@ -2,13 +2,22 @@
 
 //[GAME]==========================================================================
 void createListGame(ListGame &G){
+// IS : ListGame G belum terinisialisasi.
+// FS : ListGame G menjadi list kosong (first dan last bernilai NULL).
+
     G.first = nullptr;
     G.last = nullptr;
 }
 bool isEmptyGame(ListGame G){
+// IS : ListGame G belum terinisialisasi.
+// FS : ListGame G menjadi list kosong (first dan last bernilai NULL).
+
     return G.first == nullptr && G.last == nullptr;
 }
 adrGame createElementGame(Game gameInfo){
+// IS : Data game (gameInfo) terdefinisi.
+// FS : Menghasilkan satu elemen game baru dengan info sesuai gameInfo.
+
     adrGame p = new elemenGame;
     p->info = gameInfo;
     p->info.jumlahKarakter = 0;
@@ -18,6 +27,9 @@ adrGame createElementGame(Game gameInfo){
     return p;
 }
 void addGame(ListGame &G, adrGame game){
+// IS : ListGame G terdefinisi, adrGame game terdefinisi.
+// FS : Game ditambahkan ke dalam list game G.
+
     adrGame p = G.first;
     if (isEmptyGame(G)){
         G.first = game;
@@ -44,6 +56,9 @@ void addGame(ListGame &G, adrGame game){
     }
 }
 bool isValidGameToAdd(ListGame G, Game gameInfo){
+// IS : ListGame G dan data game (gameInfo) terdefinisi.
+// FS : Mengembalikan false jika game dengan nama yang sama sudah ada, dan true jika belum ada dalam ListGame G.
+
     adrGame p;
     if (isEmptyGame(G)){
         return true;
@@ -58,6 +73,9 @@ bool isValidGameToAdd(ListGame G, Game gameInfo){
     return true;
 }
 adrGame findGame(ListGame G, string gameName){
+// IS : ListGame G dan nama game (gameName) terdefinisi.
+// FS : Mengembalikan address game dengan nama gameName jika ditemukan, atau NULL jika tidak ditemukan.
+
     adrGame p;
     p = G.first;
     while (p != nullptr && p->info.nama != gameName){
@@ -66,6 +84,9 @@ adrGame findGame(ListGame G, string gameName){
     return p;
 }
 void deleteGame(ListGame &G, adrGame C){
+// IS : ListGame G terdefinisi, gameName terdefinisi.
+// FS : Game dengan nama gameName beserta seluruh karakternya dihapus dari list.
+
     if (C == nullptr) return;
     deleteAllCharacter(C, C->firstChar);
     if (C->prev == nullptr && C->next == nullptr){
@@ -88,9 +109,15 @@ void deleteGame(ListGame &G, adrGame C){
 
 //[CHARACTER]==========================================================================
 bool isEmptyCharacter(adrGame C){
+// IS : adrGame C terdefinisi.
+// FS : Mengembalikan true jika game tidak memiliki karakter.
+
     return C->firstChar == nullptr;
 }
 adrCharacter createElementCharacter(Character newChar){
+// IS : Data karakter (newChar) terdefinisi.
+// FS : Menghasilkan satu elemen karakter baru.
+
     adrCharacter chara;
     chara = new elemenCharacter;
     chara->info = newChar;
@@ -98,6 +125,9 @@ adrCharacter createElementCharacter(Character newChar){
     return chara;
 }
 void addCharacter(adrGame &C, adrCharacter chara){
+// IS : adrGame C dan adrCharacter chara terdefinisi.
+// FS : Karakter ditambahkan ke dalam game C.
+
     adrCharacter p;
     if (isEmptyCharacter(C)){
         C->firstChar = chara;
@@ -111,6 +141,9 @@ void addCharacter(adrGame &C, adrCharacter chara){
     C->info.jumlahKarakter = countCharacterInGame(C->firstChar);
 }
 bool isValidCharacter(adrGame C, string charaName){
+// IS : adrGame C dan nama karakter (charaName) terdefinisi.
+// FS : Mengembalikan false jika karakter dengan nama charaName sudah ada, dan true jika belum ada pada game C
+
     adrCharacter p;
     p = C->firstChar;
     while (p != nullptr){
@@ -122,6 +155,9 @@ bool isValidCharacter(adrGame C, string charaName){
     return true;
 }
 vector<adrCharacter> findCharacter(ListGame G, string charaName){
+// IS : ListGame G dan charaName terdefinisi.
+// FS : Mengembalikan address karakter dengan nama charaName jika ditemukan.
+
     vector<adrCharacter> result;
     adrGame p = G.first;
     while (p != nullptr){
@@ -137,6 +173,9 @@ vector<adrCharacter> findCharacter(ListGame G, string charaName){
     return result;
 }
 adrCharacter findCharacterFromGame(adrGame C, string charaName){
+// IS : adrGame C dan nama karakter (charaName) terdefinisi.
+// FS : Mengembalikan address karakter dengan nama charaName jika ditemukan, atau NULL jika tidak ditemukan.
+
     adrCharacter p;
     p = C->firstChar;
     while (p != nullptr){
@@ -148,6 +187,9 @@ adrCharacter findCharacterFromGame(adrGame C, string charaName){
     return nullptr;
 }
 void deleteCharacterFromGame(adrGame &C, string charaName) {
+// IS : adrGame C dan nama karakter (charaName) terdefinisi.
+// FS : Seluruh karakter dengan nama charaName dihapus dari game C dan jumlah karakter diperbarui.
+
     adrCharacter prec, chara, nextChar;
     prec = nullptr;
     chara = C->firstChar;
@@ -171,6 +213,9 @@ void deleteCharacterFromGame(adrGame &C, string charaName) {
 }
 
 void deleteAllCharacter(adrGame &C, adrCharacter &chara){
+// IS : adrGame C dan adrCharacter chara terdefinisi.
+// FS : Seluruh karakter pada game C dihapus.
+
     if (chara != nullptr){
         deleteAllCharacter(C, chara->next);
         delete chara;
@@ -196,6 +241,9 @@ void chooseCharacterArray(ListGame G, Character chosenChar[], int &count, string
     }
 }
 Character simulateFight(Character chosenChar[], int count){
+// IS : Vector chosenChar berisi satu atau lebih karakter.
+// FS : Mengembalikan karakter pemenang hasil simulasi pertarungan.
+
     int n = count;
     while (n > 1){
         Character p1 = chosenChar[n-1];
@@ -207,6 +255,9 @@ Character simulateFight(Character chosenChar[], int count){
     return chosenChar[0];
 }
 int calcDamage(Character attacker, Character defender){
+// IS : Karakter attacker dan defender terdefinisi.
+// FS : Mengembalikan nilai damage dari attacker ke defender.
+
     int baseDmg = attacker.strength;
     int expBonus = attacker.experience / 10;
     int bIQBonus = attacker.battleIQ - defender.battleIQ;
@@ -219,6 +270,9 @@ int calcDamage(Character attacker, Character defender){
     return 0;
 }
 Character duel(Character p1, Character p2){
+// IS : Dua karakter p1 dan p2 terdefinisi.
+// FS : Mengembalikan karakter pemenang duel.
+
     bool turn = true;
 
     while (p1.hp > 0 && p2.hp > 0){
@@ -257,9 +311,15 @@ void initCharaArray(ListGame G, Character charArray[], int &counter){
     }
 }
 int calcPower(Character c){
+// IS : Karakter c terdefinisi.
+// FS : Mengembalikan nilai power karakter.
+
     return (c.strength + c.hp) * c.agility * c.battleIQ + c.experience;
 }
 void sortCharacterRank(Character charArray[], int counter){
+// IS : Vector charVector terdefinisi.
+// FS : Vector karakter diurutkan berdasarkan power tertinggi.
+
     for (int i = 0; i < counter - 1; i++){
         for (int j = 0; j < counter - i - 1; j++){
             if (calcPower(charArray[j]) < calcPower(charArray[j + 1])){
@@ -274,6 +334,9 @@ void sortCharacterRank(Character charArray[], int counter){
 
 //RANK GAME
 void initGameArray(ListGame G, Game gameArray[], int &counter){
+// IS : ListGame G terdefinisi dan counter belum berisi jumlah game.
+// FS : Seluruh data game pada ListGame G disalin ke gameArray dan counter berisi jumlah game.
+
     adrGame p;
     counter = 0;
     p = G.first;
@@ -284,6 +347,9 @@ void initGameArray(ListGame G, Game gameArray[], int &counter){
     }
 }
 void sortGameWithParameter(Game gameArray[], int counter, string parameter){
+// IS : gameArray berisi counter data game dan parameter pengurutan terdefinisi.
+// FS : gameArray terurut sesuai parameter (nama, rating, atau tahun rilis).
+
     bool tukar;
     for (int i = 0; i < counter - 1; i++){
         for (int j = 0; j < counter - i - 1; j++){
@@ -304,6 +370,9 @@ void sortGameWithParameter(Game gameArray[], int counter, string parameter){
     }
 }
 adrGame GameWithMostCharacter(ListGame G){
+// IS : ListGame G terdefinisi.
+// FS : Mengembalikan address game yang memiliki jumlah karakter terbanyak.
+
     adrGame p, most;
     most = nullptr;
     int biggest = 0;
@@ -318,6 +387,9 @@ adrGame GameWithMostCharacter(ListGame G){
     return most;
 }
 int countCharacterInGame(adrCharacter chara){
+// IS : adrCharacter chara terdefinisi.
+// FS : Mengembalikan jumlah karakter dalam satu game.
+
     if (chara == nullptr){
         return 0;
     }
@@ -327,6 +399,9 @@ int countCharacterInGame(adrCharacter chara){
 
 //======================================================================================
 void byebyedata(adrGame &C){
+// IS : adrGame C terdefinisi dan dapat menunjuk ke list game.
+// FS : Seluruh game beserta seluruh karakter di dalamnya dihapus dari memori.
+
     if (C != nullptr){
         adrGame nextGame = C->next;
         deleteAllCharacter(C, C->firstChar);
